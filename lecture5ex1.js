18d0e913c02a0fa088e6de1be79fd5fce29d01d9
Dr. Frank Emanuel <emanuef@algonquincollege.com>
2025-02-03 10:08:56 -0500

    lecture examples including a media query

--- lecture5ex1.js
+++ lecture5ex1.js
@@ -4,12 +4,15 @@ var y;						    // global declared but undefined
 const PI = 3.1415;				// global constant
 
 function foo(){
-	
-	var y = 17;					// declared and assigned locally
+	console.log( "what is y? " + y);
+	y = 17;					// declared and assigned locally
+
+	console.log( "what is y? " + y);
 	{
 		let x = 'hello';		// declared and assigned locally
 		console.log( x + ' ' + y + ', ' + PI );
-		var y = 'Bob';			// declared and assigned locally
+		y = 'Bob';			// declared and assigned locally
+		console.log( "what is y? " + y);
 		a = PI;					// undeclared therefore global (hoisted)
 	}
 	
