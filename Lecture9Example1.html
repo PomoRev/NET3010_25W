301bbd4f354a5585cc919c1882003ce748e51ece
Frank Emanuel <emanuef@algonquincollege.com>
2025-03-09 18:21:28 -0400

    updated lecture 9 example files

---
+++ Lecture9Example1.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Prof. Frank Emanuel">
+	<meta name="email" content="emanuef@algonquincollege.com">
+	<meta name="date" content="2025-03-09">
+	<title>PHP Testing File</title>
+</head>
+<body>
+	<header>PHP Testing File</header>
+	<?PHP
+		echo "hello world";
+	?>
+</body>
+</html>
---
+++ Lecture9Example2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Prof. Frank Emanuel">
+	<meta name="email" content="emanuef@algonquincollege.com">
+	<meta name="date" content="2025-03-09">
+	<title>PHP Testing File #2</title>
+</head>
+<body>
+	<header>PHP Testing File #2</header>
+	<?PHP
+		
+		/* This is a comment
+		 */
+		
+		echo "hello world"; // this is also a comment
+			
+		# this is also another comment!
+			
+	?>
+</body>
+</html>
