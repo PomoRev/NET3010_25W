da29e807d8d1b7a1dc5560e67525a8a594aca621
Frank Emanuel <emanuef@algonquincollege.com>
2025-03-16 18:02:52 -0400

    started to add database parts

---
+++ crosswordDBhelper.php
@@ -0,0 +1,77 @@
+<!-- Database Helper Files -->
+
+<?php
+
+// Connecting to the Database 
+// This function requires an address for a database server that is listening for database requests. 
+// Because we are using a windows web stack (XAMPP) over the default database port id, we can 
+// simply use localhost and log in with credentials.
+
+// Alternatively you could pass in this information, but having it embedded here is fine 
+// for our purposes.
+
+function connect_to_database(){
+
+    $servername = "localhost";
+    $dbuser = "webusers";
+    $dbpassword = "password";
+
+    $connection = mysqli_connect($servername, $dbuser, $dbpassword);
+
+    if (!$connection) {
+        exit("Connection Failed: " . mysqli_connect_error());
+    }   
+    
+    return $connection;
+}
+
+function close_database_connection($connection){
+
+    mysqli_close($connection);
+
+}
+
+function execute_query($query, $connection){
+
+    return mysqli_query($connection, $query);
+
+}
+
+?>
+
+// the following code gives us an example of how to use this helper
+
+
+<?php
+
+/* echo "start<br>";
+
+$db_connection = connect_to_database();
+
+echo "<br>connected";
+
+echo "<br>query ";
+
+$myquery = "SELECT * FROM webusers.test";
+
+$results = execute_query( $myquery, $db_connection);
+
+if( mysqli_num_rows($results) > 0){
+
+    echo "<ul>";
+
+    while ($row = mysqli_fetch_assoc($results)){
+
+        echo "<li>" . $row["name"] . " = usernumber " . $row["usernumber"] . "</li>";
+
+    }
+
+    echo "</ul>";
+
+}
+
+close_database_connection( $db_connection );
+
+echo "<br>closed"; */
+
+?>
--- crosswordDDL.sql
+++ crosswordDDL.sql
@@ -5,8 +5,25 @@ DROP TABLE IF EXISTS puzzles;
 
 -- create our tables 
 
-CREATE TABLE users (
+CREATE TABLE crossword.users (
+    userID INT NOT NULL AUTO_INCREMENT , 
+    username VARCHAR(20) NOT NULL , 
+    password VARCHAR(20) NOT NULL , 
+    besttime INT NULL DEFAULT NULL , 
+    PRIMARY KEY (userID)
+);
 
-    usedID  
+CREATE TABLE crossword.puzzles (
+    puzzleID INT NOT NULL AUTO_INCREMENT , 
+    timespent INT NOT NULL , 
+    datestarted INT NOT NULL DEFAULT CURRENT_TIMESTAMP , 
+    puzzlesolution VARCHAR(400) NOT NULL , 
+    puzzleguesses VARCHAR(400) NOT NULL , 
+    acrossclues TEXT NOT NULL , 
+    downclue TEXT NOT NULL , 
+    userID INT NOT NULL ,
+    PRIMARY KEY (puzzleID),
+    FOREIGN KEY (userID) REFERENCES users(userID)
+);
 
-)
+-- If we require test data we could create some standardized test data in a DML file 
